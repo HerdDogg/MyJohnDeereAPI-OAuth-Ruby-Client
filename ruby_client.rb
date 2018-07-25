38a35110c28874511cde59b78424e9737604b105
Conroy Whitney <conroy.whitney@gmail.com>
2018-07-25 10:01:52 -0800

    Persist OAuth token/secret to send requests without needing to handshake

--- ruby_client.rb
+++ ruby_client.rb
@@ -8,7 +8,7 @@ require "oauth"
 def get_basic_catalog(token = nil)
   oauth_consumer.request(
     :get,
-    "https://sandboxapi.deere.com/platform/",
+    "#{ENV['JOHN_DEERE_API_URL']}/platform/",
     token,
     {},
     "accept" => "application/vnd.deere.axiom.v3+json"
@@ -46,7 +46,7 @@ end
 
 def oauth_params
   {
-    site: "https://sandboxapi.deere.com",
+    site: ENV["JOHN_DEERE_API_URL"],
     header: {
       Accept: "application/vnd.deere.axiom.v3+json"
     },
@@ -57,37 +57,64 @@ def oauth_params
   }
 end
 
-setup_links_for_oauth(get_basic_catalog)
+# rubocop:disable Metrics/AbcSize
+def fetch_access_token
+  setup_links_for_oauth(get_basic_catalog)
 
-# puts @current_user_uri
+  request_token = oauth_consumer(oauth_params).get_request_token({}, "oob")
 
-puts ""
-puts "***** Fetching request token *****"
+  puts "Visit this link in your browser and paste the 6 digit code here:"
+  puts request_token.authorize_url
 
-request_token = oauth_consumer(oauth_params).get_request_token({}, "oob")
-puts "Request Token received - #{request_token.token}"
-puts ""
-puts "---> Goto to url mentioned below to authorize."
-puts "---> Then paste the access token verifier"
-puts request_token.authorize_url
+  verifier = gets.chomp
 
-verifier = gets.chomp
+  new_access_token = request_token.get_access_token(oauth_verifier: verifier)
 
-puts
-puts
-puts "***** Fetching access token *****"
+  puts ""
+  puts "Paste this into your .env file:"
+  puts "JOHN_DEERE_OAUTH_SECRET=#{new_access_token.secret}"
+  puts "JOHN_DEERE_OAUTH_TOKEN=#{new_access_token.token}"
+end
 
-access_token = request_token.get_access_token(oauth_verifier: verifier)
-puts "Access token received - #{access_token.token}"
-puts
-puts "***** Fetching user details: GET /users/{userName} *****"
+def access_token
+  return nil unless existing_access_token?
 
-response = access_token.get(
-  "/platform/users/herddogg/organizations",
-  "accept" => "application/vnd.deere.axiom.v3+json"
-)
-puts
-puts "JSON Response"
-puts response.body
+  @access_token ||= OAuth::AccessToken.new(
+    oauth_consumer,
+    ENV["JOHN_DEERE_OAUTH_TOKEN"],
+    ENV["JOHN_DEERE_OAUTH_SECRET"]
+  )
+end
 
-setup_links_for_oauth(get_basic_catalog(access_token))
+def existing_access_token?
+  ENV["JOHN_DEERE_OAUTH_SECRET"] && ENV["JOHN_DEERE_OAUTH_TOKEN"]
+end
+
+def fetch_url(url, options = { method: :get })
+  return nil unless access_token
+
+  response = access_token.send(
+    options[:method],
+    "#{ENV['JOHN_DEERE_API_URL']}/platform#{url}",
+    accept: "application/vnd.deere.axiom.v3+json"
+  )
+
+  puts response.body
+end
+# rubocop:enable Metrics/AbcSize
+
+%w[JOHN_DEERE_API_KEY JOHN_DEERE_API_SECRET JOHN_DEERE_API_URL].each do |var|
+  unless ENV[var]
+    puts "Ensure that #{var} is set in your .env file"
+    exit
+  end
+end
+
+if ENV["JOHN_DEERE_OAUTH_SECRET"] && ENV["JOHN_DEERE_OAUTH_TOKEN"]
+  fetch_url(
+    "/users/herddogg/organizations",
+    method: :get
+  )
+else
+  fetch_access_token
+end
